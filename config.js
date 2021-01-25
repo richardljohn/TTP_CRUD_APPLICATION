3556340a4acd131db04f7d547461bfe9e3dfb7d7
richardljohn <richardljohn@gmail.com>
2021-01-25 03:26:29 -0500

    Stuffs

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 
+    'mongodb+srv://topTier:<vasusarecool1>@cluster0.x5dph.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    SECRET_KEY: 'an insanely secret key'
+}
